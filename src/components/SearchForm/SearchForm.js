946347982edd617d768352d43bd13f04a3ff1360
lapivoinerouge <wioladys@gmail.com>
2022-11-17 20:12:56 +0100

    Add action selectors and creators

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,24 +3,25 @@ import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
+import { updateSearchPhrase } from '../../redux/store';
 
 const SearchForm = () => {
-    const [searchInput, setSearchInput] = useState('');
-    const dispatch = useDispatch();
+  const [searchInput, setSearchInput] = useState('');
+  const dispatch = useDispatch();
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        dispatch({ type: 'UPDATE_SEARCHPHRASE', payload: searchInput })
-    }
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(updateSearchPhrase(searchInput));
+  }
 
-    return (
-        <form onSubmit={handleSubmit} className={styles.searchForm}>
-            <TextInput value={searchInput} onChange={e => setSearchInput(e.target.value)} placeholder="Search..." />
-            <Button>
-                <span className="fa fa-search" />
-            </Button>
-        </form>
-    );
-  };
+  return (
+    <form onSubmit={handleSubmit} className={styles.searchForm}>
+      <TextInput value={searchInput} onChange={e => setSearchInput(e.target.value)} placeholder='Search...' />
+      <Button>
+        <span className='fa fa-search' />
+      </Button>
+    </form>
+  );
+};
 
   export default SearchForm;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,20 @@
 import { createStore } from 'redux';
 import initialState from './initialState';
 import shortid from 'shortid';
+import { strContains } from '../utils/strContains';
+
+//selectors
+export const getFilteredCards = ({ searchPhrase, cards }, columnId) => cards
+  .filter(c => c.columnId === columnId && strContains(c.title, searchPhrase));
+
+export const getAllColumns = ({ columns }) => columns;
+
+// action creators
+export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
+
+export const addCard = payload => ({ type: 'ADD_CARD', payload });
+
+export const updateSearchPhrase = payload => ({ type: 'UPDATE_SEARCHPHRASE', payload });
 
 const reducer = (state, action) => {
   switch(action.type) {
